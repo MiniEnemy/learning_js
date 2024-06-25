df19df6d6a9920f4f0f7164a79115b064df9f839
MiniEnemy <home.minj@gmail.com>
2024-06-25 09:15:52 +0545

    learned datatpyes

---
+++ index.js
@@ -0,0 +1,70 @@
+//variables
+// var
+
+// var x =90;
+// console.log(x);
+
+// var x=100;
+// console.log(x);
+
+// //LET
+// let y=600;
+// console.log(y);
+
+//  y=1000;
+// console.log(y);
+
+// // const
+
+// const  result =90;
+// console.log(result);
+// const result = 900;
+// console.log(result);
+
+// string
+
+// const output ="aryan noob" ;
+// console.log(output);
+// console.log(typeof output);
+
+// number
+// const result =90;
+// console.log(result);
+// console.log(typeof result);
+
+//float
+
+const no=9.99;
+console.log(no);
+console.log(typeof no);
+
+
+//undefined
+let x;
+console.log(x);
+
+// nulll
+
+let y=null;
+console.log(y);
+console.log(typeof y);
+
+//boolean
+
+const istrue=true;
+console.log(istrue);
+console.log(typeof istrue);
+
+// big int
+
+const result =58565656n;
+console.log(result);
+console.log(typeof result);
+
+//symbol
+
+const sym=Symbol("hello");
+console.log(sym);
+console.log(typeof sym);
+
+//object
